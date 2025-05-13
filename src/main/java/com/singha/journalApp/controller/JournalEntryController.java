a2e0f033927fea8348d7e76f7b9fcfa23b2db89a
Cager2808 <ratikkumar28@gmail.com>
2025-05-13 10:08:07 +0530

    Implemented and Integrated MongoDB and Spring DATA operations

--- src/main/java/com/singha/journalApp/controller/JournalEntryController.java
+++ src/main/java/com/singha/journalApp/controller/JournalEntryController.java
@@ -1,8 +1,12 @@
 package com.singha.journalApp.controller;
 
 import com.singha.journalApp.entity.JournalEntry;
+import com.singha.journalApp.service.JournalEntryService;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.*;
 
@@ -10,36 +14,51 @@ import java.util.*;
 @RequestMapping("/journal")
 public class JournalEntryController {
 
-    private Map<Long, JournalEntry> journalEntries =new HashMap<>();
+    @Autowired
+    private JournalEntryService journalEntryService;
 
-    @GetMapping("/getAllEntries")
+
+
+    @GetMapping("/getAll")
     public List<JournalEntry> getAll(){
 
-        return new ArrayList<>(journalEntries.values());
+        return journalEntryService.getAll();
     }
-    @PostMapping
-    public boolean createEntry(@RequestBody JournalEntry myEntry){
 
-        journalEntries.put(myEntry.getId(),myEntry);
-    return true;
+    @PostMapping
+    public String createEntry(@RequestBody JournalEntry myEntry){
+        myEntry.setDate(LocalDateTime.now());
+        journalEntryService.saveEntry(myEntry);
+
+    return "Entry Created";
     }
 
     @GetMapping("id/{myId}")
-    public JournalEntry getJournalEntryByID(@PathVariable Long myId){
-        return journalEntries.get(myId);
+    public JournalEntry getJournalEntryByID(@PathVariable ObjectId myId){
+        return journalEntryService.findById(myId).orElse(null);
     }
 
 
     //Delete
     @DeleteMapping("id/{myId}")
-    public String deleteJournalEntryById(@PathVariable Long myId){
-        journalEntries.remove(myId);
-        return "Entry deleted";
+    public String deleteJournalEntryById(@PathVariable ObjectId myId){
+
+        journalEntryService.deleteById(myId);
+        return "Entry Deleted";
     }
 
     @PutMapping("id/{myId}")
-    public JournalEntry updateJournalEntryById(@PathVariable Long myId, @RequestBody JournalEntry myEntry){
-        return journalEntries.put(myId,myEntry);
+    public JournalEntry updateJournalEntryById(@PathVariable ObjectId myId, @RequestBody JournalEntry newEntry){
+        JournalEntry oldjournalEntry= journalEntryService.findById(myId).orElse(null);
+        if(oldjournalEntry!=null){
+            oldjournalEntry.setTitle(newEntry.getTitle()!=null && !newEntry.getTitle().equals("")?newEntry.getTitle():oldjournalEntry.getTitle());
+            oldjournalEntry.setContent(newEntry.getContent()!=null && !newEntry.getContent().equals("")? newEntry.getContent() : oldjournalEntry.getContent());
+
+        }
+        journalEntryService.saveEntry(
+                oldjournalEntry
+        );
+        return oldjournalEntry;
     }
 
 }
---
+++ src/main/java/com/singha/journalApp/repository/JournalEntryRepository.java
@@ -0,0 +1,9 @@
+package com.singha.journalApp.repository;
+
+import com.singha.journalApp.entity.JournalEntry;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface JournalEntryRepository extends MongoRepository<JournalEntry, ObjectId> {
+
+}
